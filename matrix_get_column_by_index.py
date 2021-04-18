24096dfca91cb1eb1fc01315bb6da181611e4014
Tofuni <fran@fbacarisas.xyz>
2021-04-18 11:50:11 -0400

    fix index search in matrix_get_column_by_index, update tests

--- matrix_get_column_by_index.py
+++ matrix_get_column_by_index.py
@@ -1,7 +1,7 @@
 # params: (matrix) 2d array, (index) an integer index
 def get_column_by_index(matrix, index):
 	col = []
-	if index not in range(len(matrix)):
+	if index not in range(len(matrix[0])):
 		return None
 	for row in matrix:
 		col.append(row[index])
@@ -13,11 +13,8 @@ a = [['a','b','c','d','e','f','g','h'],
 	[1,2,3,4,5,6,7,8],
 	[9,10,11,12,13,14,15,16],
 	[17,18,19,20,21,22,23,24],
-	[25,26,27,28,29,30,31,32],
-	[33,34,35,36,37,38,39,40],
-	[41,42,43,44,45,46,47,48],
-	[49,50,51,52,53,54,55,56]]
-indices = [0,2,4,6,8,1,3,5,7,9]
+	[25,26,27,28,29,30,31,32]]
+indices = [0,2,4,6,1,3,5,7,9]
 
 print("\nmatrix\n")
 for i in a:
