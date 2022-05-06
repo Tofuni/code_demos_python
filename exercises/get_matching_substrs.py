94d44546d29f437d5165d3756f07a593454de399
senaonp <71605688+senaonp@users.noreply.github.com>
2022-05-05 23:17:42 -0400

    refactor get_matching_substrs

--- exercises/get_matching_substrs.py
+++ exercises/get_matching_substrs.py
@@ -3,18 +3,17 @@ def matching_substr(txt1, txt2):
 	for i in range(len(txt1)):
 		for j in range(len(txt2)):
 			if txt1[i] == txt2[j]:
+				r.append(txt1[i])
 				isMatch = True
 				count = 1
 				while isMatch:
 					isMatch = False
-					if i+count > len(txt1) or j+count > len(txt2):
+					if i+count == len(txt1) or j+count == len(txt2):
 						break
-					c1 = txt1[i:i+count]
-					c2 = txt2[j:j+count]
-					if c1 == c2:
+					if txt1[i+count] == txt2[j+count]:
 						isMatch = True
 						count += 1
-						r.append(c1)
+						r.append(txt1[i:i+count])
 	return r
 
 print(matching_substr("catfish sands", "cats and fish"))
